d3defd0dd025a3d5ffb6845d8fa948cf5e56bd40
Wesley Vila Seca <wesley.vilaseca@hotmail.com>
2024-08-10 13:50:33 -0300

    chore: form de visualização do livro

--- resources/views/admin/books/show.blade.php
+++ resources/views/admin/books/show.blade.php
@@ -1,10 +1,25 @@
 @extends('layouts.admin')
 @section('content')
     <div class="container-fluid">
-        <form action="{{ $action }}" method="post" enctype="multipart/form-data">
+        <form action="{{ $action }}" method="POST" id="booksForm">
             @csrf
             @method('DELETE')
             @include('admin.books._partials.form')
         </form>
     </div>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var form = document.getElementById('booksForm');
+            var elements = form.elements;
+            
+            for (var i = 0; i < elements.length; i++) {
+                if (elements[i].id == 'bookButtonSubmit' || elements[i].name == '_token' || elements[i].name == '_method') {
+                    continue;
+                }
+    
+                elements[i].disabled = true;
+            }
+        });
+    </script>
 @stop
