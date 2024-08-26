649f4fbe1d29fe9b99e6766769cacc0bdc1073a8
Wesley Vila Seca <wesley.vilaseca@hotmail.com>
2024-08-26 01:11:11 -0300

    chore: ajuste css footer padrão

--- themes/codevila/blocks/footer-default/config.php
+++ themes/codevila/blocks/footer-default/config.php
@@ -4,4 +4,15 @@ return [
     'title' => 'Footer padrão',
     'category' => 'General',
     'icon' => 'fa fa-bars',
+    'fields' => [
+        'title' => [
+            'label' => 'Título',
+            'type' => 'text',
+        ],
+        'content' => [
+            'label' => 'Conteúdo',
+            'type' => 'textarea',
+        ]
+    ],
+    'settings' => []
 ];
