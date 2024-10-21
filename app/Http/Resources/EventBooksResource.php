d38a3fa8d94af3bd6bf2c840addcaeb9edf43d82
Wesley Vila Seca <wesley.vilaseca@hotmail.com>
2024-10-21 12:19:41 -0300

    fix: links livros

--- app/Http/Resources/EventBooksResource.php
+++ app/Http/Resources/EventBooksResource.php
@@ -15,13 +15,18 @@ class EventBooksResource extends JsonResource
      */
     public function toArray($request)
     {
+        if()
         return [
             'name' => $this->name,
             'subject' => $this->subject ?? '',
             'isbn' => $this->isbn ?? '',
             'price' => $this->price ? 'R$ ' . Utils::numberFormat($this->price) : '',
             'price_discount' => $this->price_discount ? 'R$ ' . Utils::numberFormat($this->price_discount) : '',
-            'link' => $this->link ? 'https://' . $this->link : '',
+            'link' => $this->link 
+                ? (str_starts_with($this->link, 'http://') || str_starts_with($this->link, 'https://') 
+                    ? $this->link 
+                    : 'https://' . $this->link) 
+                    : '',            
             'authors' => $this->author,
             'publisher' => $this->publishers[0]->name ?? ''
             // 'url' => $this->url ?? '',
