1cfe96a476314c949c7e137e01fd1cca86ce5848
Wesley Vila Seca <wesley.vilaseca@hotmail.com>
2024-10-21 12:22:26 -0300

    fix: links livros

--- app/Http/Resources/EventBooksResource.php
+++ app/Http/Resources/EventBooksResource.php
@@ -15,7 +15,6 @@ class EventBooksResource extends JsonResource
      */
     public function toArray($request)
     {
-        if()
         return [
             'name' => $this->name,
             'subject' => $this->subject ?? '',
